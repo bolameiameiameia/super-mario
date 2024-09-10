82788dfa3ee3a90c65026402db4bcecfd2b1fedc
bola666 <163007758+bolameiameiameia@users.noreply.github.com>
2024-09-10 08:29:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe");
+const startButton = document.querySelector(".start");
+const gameOverScreen = document.querySelector(".game-over");
 
 const jump = () => {
     mario.classList.add("jump");
